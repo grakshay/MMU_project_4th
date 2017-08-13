5b178b734a118a651b1cfcf965c020cf4b0679cb
grakshay <garg.akshay393@gmail.com>
2017-08-13 13:16:13 +0530

    new default user

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 print ("let's get started")
-spy_name = raw_input("type your name here")
-spy_salutation = raw_input("what should we call you ")
-spy_name = spy_salutation + ' ' + spy_name
-print 'welcome '   + spy_name +    " glad to have you back with us"
+spy_name = raw_input("type your name here ")
+if len(spy_name) > 0 :
+    spy_age = 0
+    spy_rating = 0.0
+    spy_is_online = False
+    spy_salutation = raw_input("what should we call you ")
+    spy_age = raw_input("what is your age  ")
+    print type(spy_age)
+    spy_age =  float(int(spy_age))
+    print type(spy_age)
+    spy_name = spy_salutation + ' ' + spy_name
+    print 'welcome ' + spy_name + " glad to have you back with us"
+    print "alright " + spy_name + " i'd like to know more about you before we proceed further"
+else:
+    print "invalid name....please try again"
+
+
---
+++ spy_details.py
@@ -0,0 +1,4 @@
+spy_name = "bond"
+spy_salutation = "mr"
+spy_age = 21
+spy_ratings = 5
